3df3a46425423583aa9e892fd64bdd3f316fec32
Gatocan12 <dani.sanchez.marzoa@outllok.com>
2022-01-20 21:31:30 +0100

    Zsh config

--- install.sh
+++ install.sh
@@ -4,6 +4,15 @@ dnf install neovim
 dnf install stow
 dnf install gpg
 dnf install pass
+dnf install zsh
+dnf install util-linux-user
+
+# add zsh to valid login shells
+command -v zsh | udo ee -a /etc/shells
+# set zsh as default shell
+sudo chsh -s $(which zsh) $USER
+# install oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # stow dotfiles
 stow git
